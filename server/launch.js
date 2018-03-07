3a24c6f2039697293aa25a1e9b299061633f6a3d
Marc Littlemore <marc.littlemore@bbc.co.uk>
2018-03-07 15:58:53 +0000

    Add Express server

---
+++ server/launch.js
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => res.json({text: 'Hello World!'}));
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
