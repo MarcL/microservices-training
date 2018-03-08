8b416afdeabff4abefe250a7923bf84cf03492b2
Marc Littlemore <marc.littlemore@bbc.co.uk>
2018-03-08 09:54:34 +0000

    Refactor middleware

---
+++ server/middleware/userCreate.js
@@ -0,0 +1,33 @@
+import event from '../event';
+import validateUserData from '../validateUserData';
+import callGateway from '../callGateway';
+
+const userCreate = (request, response) => {
+    const userData = request.body;
+
+    if (validateUserData(userData)) {
+        const { email, password } = userData;
+
+        const payload = {
+            email,
+            password,
+        };
+
+        const newEvent = event(userData, 'user/create', payload);
+
+        return callGateway('http://localhost:4000/users', newEvent)
+            .then(() => {
+                response.json({ success: true });
+            })
+            .catch((error) => {
+                response.json({ success: false, error: error.message });
+            });
+    }
+
+    return response.json({
+        success: false,
+        error: 'Aggregate root failure (validation failed)',
+    });
+};
+
+export default userCreate;
--- server/service1.js
+++ server/service1.js
@@ -1,40 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import event from './event';
-import validateUserData from './validateUserData';
-import callGateway from './callGateway';
+import userCreate from './middleware/userCreate';
 
 const service1 = (port) => {
     const app = express();
     app.use(bodyParser.json());
 
-    app.post('/', (request, response) => {
-        const userData = request.body;
-
-        if (validateUserData(userData)) {
-            const { email, password } = userData;
-
-            const payload = {
-                email,
-                password,
-            };
-
-            const newEvent = event(userData, 'user/create', payload);
-
-            return callGateway('http://localhost:4000/users', newEvent)
-                .then(() => {
-                    response.json({ success: true });
-                })
-                .catch((error) => {
-                    response.json({ success: false, error: error.message });
-                });
-        }
-
-        return response.json({
-            success: false,
-            error: 'Aggregate root failure (validation failed)',
-        });
-    });
+    app.post('/', userCreate);
 
     app.listen(port, () => console.log(`Listening on port : ${port}`));
 
